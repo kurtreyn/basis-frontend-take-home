38fae70ebeb8fb66e751355d646939c2d639df0b
kurtreyn <kurtreyn@yahoo.com>
2022-05-21 13:20:04 -0400

    finished setting up filter functionality

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,28 +1,24 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
+import DatePicker from 'react-datepicker';
 import {
   setTotalStartDates,
   setTotalEndDates,
   setTotalImpressions,
-  setTotalCostPerMile,
   setTotalAllCost,
 } from '../redux/actions';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function Filter() {
-  const {
-    // totalStartDates,
-    // totalEndDates,
-    // totalImpressions,
-    // totalCostPerMile,
-    // totalAllCost,
-
-    placements,
-  } = useSelector((state) => state.Reducer);
+  const { totalCostPerMile, placements } = useSelector(
+    (state) => state.Reducer
+  );
   const dispatch = useDispatch();
   const [fromDate, setFromDate] = useState(null);
   const [toDate, setToDate] = useState(null);
-
+  // console.log('fromDate', fromDate);
+  // console.log('toDate', toDate);
   let deliveryArr = placements.map((placement) => {
     return placement.delivery.map((item) => {
       return item;
@@ -31,14 +27,17 @@ function Filter() {
   deliveryArr = deliveryArr.flat();
   let dateArr = deliveryArr.map((item) => item.date);
   let impressionsArr = deliveryArr.map((item) => item.impressions);
+  // console.log('impressionCost', impressionCost);
+  // console.log('costPerMile ', costPerMile);
+  // console.log('totalCost ', totalCost);
 
   const handleClick = (from, to) => {
     console.log('clicked');
     from = new Date(from).toLocaleDateString('en-US');
     to = new Date(to).toLocaleDateString('en-US');
-    let impressTotal = 0;
-    let costPerMileTotal = 0;
-    let totalAll = 0;
+    // let impressTotal = 0;
+    // let costPerMileTotal = 0;
+    // let totalAll = 0;
     for (let date of dateArr) {
       if (dateArr.indexOf(from) === -1 || dateArr.indexOf(to) === -1) {
         // console.log('from', from);
@@ -54,7 +53,6 @@ function Filter() {
     let begin = dateArr.indexOf(from);
     let end = dateArr.indexOf(to);
     let addingArrImpress = [];
-    let addingArrCpm = [];
     let addingArrTotalCost = [];
     for (let i = 0; i < impressionsArr.length; i++) {
       if (i >= begin && i <= end) {
@@ -65,20 +63,31 @@ function Filter() {
       (accum, curVal) => accum + curVal,
       0
     );
-    console.log('from', from);
-    console.log('to', to);
-    console.log('begin', begin);
-    console.log('end', end);
-    console.log('addingArr', addingArrImpress);
-    console.log('addingSum', addingSumImpress);
-    console.log('addingArrCpm', addingArrCpm);
-    console.log('addingArrTotalCost', addingArrTotalCost);
-    console.log('impressTotal', impressTotal);
-    console.log('costPerMileTotal', costPerMileTotal);
-    console.log('totalAll', totalAll);
+    for (let i = 0; i < addingArrImpress.length; i++) {
+      addingArrTotalCost.push(Math.round(addingArrImpress[i]));
+    }
+    let addingSumCost = addingArrTotalCost.reduce(
+      (accum, curVal) => accum + curVal,
+      0
+    );
+    let updatedTotalCost = Math.round(
+      (addingSumCost / 1000) * totalCostPerMile
+    );
+    // console.log('from', from);
+    // console.log('to', to);
+    // console.log('begin', begin);
+    // console.log('end', end);
+    // console.log('addingArr', addingArrImpress);
+    // console.log('addingSum', addingSumImpress);
+    // console.log('addingArrCpm', addingArrCpm);
+    // console.log('addingArrTotalCost', addingArrTotalCost);
+    // console.log('impressTotal', impressTotal);
+    // console.log('costPerMileTotal', costPerMileTotal);
+    // console.log('totalAll', totalAll);
     dispatch(setTotalStartDates(from));
     dispatch(setTotalEndDates(to));
     dispatch(setTotalImpressions(addingSumImpress));
+    dispatch(setTotalAllCost(updatedTotalCost));
   };
 
   return (
@@ -100,20 +109,28 @@ function Filter() {
       </div>
       <div className="row">
         <div className="col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2 filter-input-col">
-          <input
+          <DatePicker
+            selected={fromDate}
+            onChange={(date) => setFromDate(date)}
+          />
+          {/* <input
             className="form-input"
             type="date"
             id="start-date"
             onChange={(e) => setFromDate(e.target.value)}
-          />
+          /> */}
         </div>
         <div className="col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2 filter-input-col">
-          <input
+          <DatePicker
+            selected={fromDate}
+            onChange={(date) => setToDate(date)}
+          />
+          {/* <input
             className="form-input"
             type="date"
             id="end-date"
             onChange={(e) => setToDate(e.target.value)}
-          />
+          /> */}
         </div>
         <div className="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8 filter-input-col">
           <Button onClick={() => handleClick(fromDate, toDate)}>Apply</Button>
